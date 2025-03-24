23420ba6647d8e48dd4e303cc72225b9f5c337dd
RyanO232010 <ryano92@nycstudents.net>
2025-03-24 09:25:35 -0400

    1

---
+++ app.py
@@ -0,0 +1,15 @@
+while True:
+    mode = input("Would you like to enter teacher mode or student mode? ")
+
+
+
+    #TEACHER MODE
+    if mode.lower() == "teacher mode":
+        print("Ok")
+
+    #STUDENT MODE
+    elif mode.lower() == "student mode":
+        print("loser")
+
+    else:
+        print("Invalid response. Please try again.")
