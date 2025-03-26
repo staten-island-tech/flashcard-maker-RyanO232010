d2b88f6f40839bd64ce17a6a08f5318370fd9e92
RyanO232010 <ryano92@nycstudents.net>
2025-03-26 09:10:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+class Teacher :
+     
+
+
+
+
+
 while True:
     mode = input("Would you like to enter teacher mode or student mode? ")
 
 
 
     #TEACHER MODE
-    if mode.lower() == "teacher mode":
-        print("Ok")
+    """ if mode.lower() == "teacher mode":
+        
 
     #STUDENT MODE
     elif mode.lower() == "student mode":
         print("loser")
-
+ """
     else:
         print("Invalid response. Please try again.")
