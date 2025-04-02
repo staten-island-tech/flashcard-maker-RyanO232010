eaaa157cb7852bf46099c8ef7bdf3c70261ac914
RyanO232010 <ryano92@nycstudents.net>
2025-04-02 09:02:31 -0400

    1

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-class Teacher :
-     
+import json
 
-
-
-
-
-while True:
-    mode = input("Would you like to enter teacher mode or student mode? ")
-
-
-
-    #TEACHER MODE
-    """ if mode.lower() == "teacher mode":
-        
-
-    #STUDENT MODE
-    elif mode.lower() == "student mode":
-        print("loser")
- """
-    else:
-        print("Invalid response. Please try again.")
+class Teacher:
+    def __init__(self,words,definition):
+        self.words = words
+        self.definition = definition
