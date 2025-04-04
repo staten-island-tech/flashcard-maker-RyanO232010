b7bdf53fb8b2bf7f14f91124cbfb582ff27d94a9
RyanO232010 <ryano92@nycstudents.net>
2025-04-04 09:23:58 -0400

    1

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+Role = input("What role are you? ")
+Question = input("What is the term? ")
+Answer = input("What is the defintion? ")
+
+
 import json
 
-class Teacher:
+class Flashcard:
     def __init__(self,words,definition):
         self.words = words
         self.definition = definition
+    def to_dict(self):
+        return {"make": self.words, "model": self.definition}
+
+
+    with open("FlashCards.json", "w") as file:
+        json.dump(flashcards_data, file, indent=4)
+
+
